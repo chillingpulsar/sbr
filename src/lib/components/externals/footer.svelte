7e23de7ab779f8064f6d998236fb339706addf77
Mike John Eviota <mikejohneviota@Mikes-Mac-mini.local>
2025-10-18 11:50:56 +0800

    add landing page generate by AI vibe coding easy coding no learning kidding
    update git ignore

---
+++ src/lib/components/externals/footer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import IconLink from '@tabler/icons-svelte/icons/link';
+	import IconHandFingerLeft from '@tabler/icons-svelte/icons/hand-finger-left';
+</script>
+
+<footer class="border-t bg-secondary">
+	<section
+		class="container mx-auto flex min-h-[150px] flex-wrap items-center justify-between gap-4 p-4"
+	>
+		<div class="flex items-center gap-1">
+			<p class="text-sm text-muted-foreground">Made with ❤️ by</p>
+			<a
+				target="_blank"
+				href="https://github.com/chillingpulsar"
+				class="flex items-center gap-1 text-sm text-muted-foreground underline"
+			>
+				<b>Mike</b> John
+				<IconHandFingerLeft class="size-4 animate-bounce" />
+			</a>
+		</div>
+	</section>
+</footer>
